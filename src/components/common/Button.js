110360bbd0f4242eecc71eff6fc6592549266631
Raphael Croce <rcroce7@gmail.com>
2019-03-12 00:56:44 -0500

    Implemented reusable input component and made small change to button component

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -14,7 +14,7 @@ const Button = ({onPress, children}) => {
 const styles = {
   textStyle: {
     alignSelf: 'center',
-    color: '007aff',
+    color: '#007aff',
     fontSize: 16,
     fontWeight: '600',
     paddingTop: 10,
