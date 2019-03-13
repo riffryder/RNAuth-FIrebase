49f37d1d377a7ffe42761ef84b62f5a0bbbcabf8
Raphael Croce <rcroce7@gmail.com>
2019-03-12 23:34:35 -0500

    Set up Firebase state listening to render log in/out button based on state

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
 
-import { Header } from './components/common';
+import { CardSection, Button, Header, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 import {
   apiKey,
@@ -14,6 +14,10 @@ import {
 } from '../config'
 
 class App extends Component {
+  state = {
+    loggedIn: null
+  }
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey, 
@@ -23,13 +27,41 @@ class App extends Component {
       storageBucket,
       messagingSenderId,
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({
+          loggedIn: true
+        });
+      } else {
+        this.setState({
+          loggedIn: false
+        });
+      }
+    });
   }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <CardSection>
+            <Button onPress={() => firebase.auth().signOut()}>Log Out</Button>
+          </CardSection>
+          );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner />;
+    };
+  }
+
 
   render() {
     return(
       <View style={styles.container}>
         <Header headerText="authentication"></Header>
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
